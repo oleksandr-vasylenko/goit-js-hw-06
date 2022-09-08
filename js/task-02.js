95ae2d7550d49ab230d1228912972456476b83b7
Oleksandr Vasylenko <oleksandr.vasylenko.ua@gmail.com>
2022-09-08 16:14:14 +0300

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,12 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+for (const item of ingredients) {
+  const itemEl = document.createElement('li');
+  itemEl.classList.add('item');
+  itemEl.textContent = item;
+
+  const listEl = document.querySelector('#ingredients');
+  listEl.appendChild(itemEl);
+}
