1faaa8f0e012f17f4180a7df131cf38f9df3536b
Oleksandr Vasylenko <oleksandr.vasylenko.ua@gmail.com>
2022-09-08 16:18:24 +0300

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -7,6 +7,7 @@ const ingredients = [
   'Condiments',
 ];
 
+// --- "for of" option
 for (const item of ingredients) {
   const itemEl = document.createElement('li');
   itemEl.classList.add('item');
@@ -15,3 +16,12 @@ for (const item of ingredients) {
   const listEl = document.querySelector('#ingredients');
   listEl.appendChild(itemEl);
 }
+
+// --- .map option
+// ingredients.map(item => {
+//   const itemEl = document.createElement('li');
+//   itemEl.classList.add('item');
+//   itemEl.textContent = item;
+//   const listEl = document.querySelector('#ingredients');
+//   listEl.appendChild(itemEl);
+// });
