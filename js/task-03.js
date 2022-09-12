bfe1269ec8ad8e33266ef6aa9e8450d726c78c01
Oleksandr Vasylenko <oleksandr.vasylenko.ua@gmail.com>
2022-09-12 18:41:23 +0300

    Completes task #03

--- js/task-03.js
+++ js/task-03.js
@@ -1,16 +1,3 @@
-// Завдання 3
-// Напиши скрипт для створення галереї зображень
-// на підставі масиву даних.HTML містить список ul.gallery.
-
-// <ul class="gallery"></ul>
-
-// Використовуй масив об'єктів images для створення елементів <img>,
-// вкладених в < li >.Для створення розмітки
-// використовуй шаблонні рядки і метод insertAdjacentHTML().
-
-// - Усі елементи галереї повинні додаватися в DOM за одну операцію додавання.
-// - Додай мінімальне оформлення галереї флексбоксами або грідами через CSS класи.
-
 const images = [
   {
     url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
@@ -26,67 +13,13 @@ const images = [
   },
 ];
 
-// ------ function 01
-// const makeGalleryRef = images => {
-//   const { url, alt } = images;
-//   return `<ul class="gallery"><li><images src="${url}" alt="${alt}"></images></li>`;
-// };
+const listRef = document.querySelector('.gallery');
 
-// const galleryRef = document.querySelector('.gallery');
-// galleryRef.append(...images);
+const itemRef = images
+  .map(
+    image =>
+      `<li class="list-item"><img src="${image.url}"alt ="${image.alt}"width=320></img></li>`
+  )
+  .join('');
 
-// ------ for of
-for (const item of images) {
-  const itemRef = document.createElement('li');
-
-  const imgRef = document.createElement('img');
-  imgRef.src = item.url;
-  imgRef.alt = item.alt;
-  imgRef.width = 640;
-
-  itemRef.appendChild(imgRef);
-  const listEl = document.querySelector('.gallery');
-  listEl.style.listStyle = 'none';
-  listEl.style.display = 'flex';
-  listEl.style.flexDirection = 'column';
-  listEl.style.alignItems = 'center';
-  listEl.style.gap = '20px';
-
-  listEl.appendChild(itemRef);
-}
-
-// ------ map - 01
-// images.map(item => {
-//   const listRef = document.createElement('li');
-//   const imgRef = document.createElement('img');
-//   imgRef.src = item.url;
-//   imgRef.alt = item.alt;
-//   imgRef.width = 640;
-//   listRef.appendChild(imgRef);
-
-//   console.log(`text: ${listRef.textContent}`);
-
-//   const galRef = document.querySelector('.gallery');
-//   galRef.insertAdjacentHTML('beforebegin', `${listRef}`);
-// });
-
-// ------ map - 02
-// for (const item of images) {
-//   const itemRef = document.createElement('li');
-
-//   const imgRef = document.createElement('img');
-//   imgRef.src = item.url;
-//   imgRef.alt = item.alt;
-//   imgRef.width = 640;
-
-//   itemRef.appendChild(imgRef);
-//   const listRef = document.querySelector('.gallery');
-//   listRef.style.listStyle = 'none';
-//   listRef.style.display = 'flex';
-//   listRef.style.flexDirection = 'column';
-//   listRef.style.alignItems = 'center';
-//   listRef.style.gap = '20px';
-
-//   listRef.insertAdjacentHTML('beforebegin', itemRef);
-//   //   listEl.appendChild(itemRef);
-// }
+listRef.insertAdjacentHTML('afterbegin', itemRef);
