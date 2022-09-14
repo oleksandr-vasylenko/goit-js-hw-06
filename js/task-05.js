995f84e7de7221ac894385b1e4e6d0a2ae3b0053
Oleksandr Vasylenko <oleksandr.vasylenko.ua@gmail.com>
2022-09-14 14:29:25 +0300

    Completes task #05

--- js/task-05.js
+++ js/task-05.js
@@ -1,1 +1,7 @@
-console.log('hello');
+const input = document.querySelector('input#name-input');
+const user = document.querySelector('span#name-output');
+
+input.addEventListener(
+  'input',
+  event => (user.textContent = event.currentTarget.value)
+);
