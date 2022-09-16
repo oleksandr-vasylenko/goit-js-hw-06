693232681e652150355bd62db6d5f4cc1c3340c3
Oleksandr Vasylenko <oleksandr.vasylenko.ua@gmail.com>
2022-09-16 19:53:07 +0300

    Completes task #08

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,21 @@
-console.log('hello');
+const loginRef = document.querySelector('.login-form');
+
+loginRef.addEventListener('submit', onClick);
+
+function onClick(event) {
+  event.preventDefault();
+  if (
+    event.currentTarget.elements.email.value === '' ||
+    event.currentTarget.elements.password.value === ''
+  ) {
+    alert('Come on, Man fill in the email and the password 😉');
+  }
+
+  const { email, password } = event.currentTarget.elements;
+  const userData = {
+    email: email.value,
+    password: password.value,
+  };
+  console.log(userData);
+  event.currentTarget.reset();
+}
